19693f376a6f84a453da145c6475d610016f71d1
ghosantos <gustavo.santos7611@gmail.com>
2026-03-23 23:23:55 -0300

    feat: Spring Boot project initialization

---
+++ itau-challenge/src/test/java/com/ghosantos/backend/itau/challenge/ItauChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghosantos.backend.itau.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
