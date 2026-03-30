80be7fe04c99d3e4b83f11dc51bfe8e360be74f5
ghosantos <gustavo.santos7611@gmail.com>
2026-03-30 13:41:15 -0300

    feat: implementação de Bean Validation (JSR-303) no TransactionRequest

--- itau-challenge/src/main/java/com/ghosantos/backend/itau/challenge/dto/TransactionRequest.java
+++ itau-challenge/src/main/java/com/ghosantos/backend/itau/challenge/dto/TransactionRequest.java
@@ -1,7 +1,18 @@
 package com.ghosantos.backend.itau.challenge.dto;
 
-import java.io.Serializable;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
 import java.time.OffsetDateTime;
 
-public record TransactionRequest(Double value, OffsetDateTime dateTime) implements Serializable {
-}
+public record TransactionRequest(
+
+        @NotNull(message = "Campos obrigatórios não foram preenchidos: value")
+        @Positive(message = "O valor deve ser maior que zero")
+        Double value,
+
+        @NotNull(message = "Campos obrigatórios não foram preenchidos: dateTime")
+        @PastOrPresent(message = "A data não pode ser no futuro")
+        OffsetDateTime dateTime
+) { }
