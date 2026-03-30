2b00304e96015337918609140e8d38a5061a80b8
ghosantos <gustavo.santos7611@gmail.com>
2026-03-30 13:43:17 -0300

    fix: padronização do retorno HTTP 422 via GlobalExceptionHandler

--- itau-challenge/src/main/java/com/ghosantos/backend/itau/challenge/controller/exceptions/GlobalExceptionHandler.java
+++ itau-challenge/src/main/java/com/ghosantos/backend/itau/challenge/controller/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,10 @@
 package com.ghosantos.backend.itau.challenge.controller.exceptions;
 
-import com.ghosantos.backend.itau.challenge.exceptions.FutureDateNotAllowedException;
 import com.ghosantos.backend.itau.challenge.exceptions.PastDateNotAllowedException;
-import com.ghosantos.backend.itau.challenge.exceptions.UnfilledFieldsException;
-import com.ghosantos.backend.itau.challenge.exceptions.InvalidValueException;
-
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,28 +12,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(FutureDateNotAllowedException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public void handlerDateHourPosteriors(FutureDateNotAllowedException e) {
+    public void handleValidationError(MethodArgumentNotValidException e) {
     }
 
     @ExceptionHandler(PastDateNotAllowedException.class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public void handlerDateHourPrevious(PastDateNotAllowedException e) {
-    }
-
-    @ExceptionHandler(UnfilledFieldsException.class)
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public void handlerUnfilledFields(UnfilledFieldsException e) {
-    }
-
-    @ExceptionHandler(InvalidValueException.class)
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public void handlerValueInvalid(InvalidValueException e) {
+    public void handlePastDateError(PastDateNotAllowedException e){
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void handlerMessageNotReadable(HttpMessageNotReadableException e){
+    public void handleBadRequest(HttpMessageNotReadableException e) {
     }
 }
